6415dd26d21441af6e425d33974ad48fa0edc7b9
xTirian <tirianx@yahoo.com>
2023-12-29 16:35:33 -0300

    feat: config database

--- backend-mberti/src/database/config/mongo.ts
+++ backend-mberti/src/database/config/mongo.ts
@@ -1,6 +1,15 @@
 import mongoose from 'mongoose'
 
-mongoose.connect(`mongodb://localhost:27017/local`)
+const getUrl : () => string = () => {
+  if(process.env.MONGO_DATABASE_URL && typeof process.env.MONGO_DATABASE_URL == "string"){
+    return process.env.MONGO_DATABASE_URL
+  }
+  return ""
+}
+
+let url = getUrl()
+
+mongoose.connect("mongodb://localhost:27017/local")
 
 
 export default mongoose;
