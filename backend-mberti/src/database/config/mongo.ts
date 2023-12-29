6945753a6091f87d42a7f78076d1aa52558bcabe
xTirian <tirianx@yahoo.com>
2023-12-29 16:25:50 -0300

    feat: config database

--- backend-mberti/src/database/config/mongo.ts
+++ backend-mberti/src/database/config/mongo.ts
@@ -1,19 +1,6 @@
-import dotenv from "dotenv";
-dotenv.config();
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const dbConnection : () => string = function () : string {
-
-  let url = process.env.MONGO_CONNECTION_URL;
-
-  if (typeof url == "string") {
-    return url
-  }
-
-  return ""
-};
-
-  mongoose.connect(dbConnection());
+mongoose.connect(`mongodb://localhost:27017/local`)
 
 
 export default mongoose;
