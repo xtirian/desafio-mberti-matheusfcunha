2410edb8a617cdc2cc35edac87eb5e2e8b03ab6e
xTirian <tirianx@yahoo.com>
2023-12-29 22:24:13 -0300

    add map to components

--- frontend-mberti/src/app/(pages)/(home)/page.tsx
+++ frontend-mberti/src/app/(pages)/(home)/page.tsx
@@ -1,14 +1,39 @@
-import Image from 'next/image'
-import styles from './page.module.scss'
-import { AddButton, DeleteButton } from '@/components/button'
-import SubjectCard from '@/components/card'
+'use client'
+import './style.scss'
+import SubjectContainer from '@/components/subject'
+import Modal from '@/view/modal'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <main>
-      {/* TODO: COLOCAR O LOADING NO FINAL */}
 
-      <SubjectCard />
+  const elementos = [];
+  
+  for(let i = 1; i <= 4; i++){
+    elementos.push(<SubjectContainer key={i} selectBimestre={i} />)
+  }
+
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <main className='mainContainer'>
+      {/* TODO: COLOCAR O LOADING NO FINAL */}
+      
+      {elementos}
+
+      {/* MODAL */}
+
+      <Modal isOpen={isModalOpen} onClose={() => closeModal} />
+
+     
     </main>
   )
 }
